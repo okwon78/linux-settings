773b831a057754e18ed7e26dd11cf2b53b1ce644
obkwon <okwon78@gmail.com>
2019-02-14 14:06:09 +0900

    add git.sh

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew cask install diffmerge
+
+git config --global user.name obkwon
+git config --global user.email okwon78@gmail.com
+git config --global diff.tool diffmerge
+git config --global difftool.diffmerge.cmd 'diffmerge "$LOCAL" "$REMOTE"'
+git config --global merge.tool diffmerge
+git config --global mergetool.diffmerge.cmd 'diffmerge --merge --result="$MERGED" "$LOCAL" "$(if test -f "$BASE"; then echo "$BASE"; else echo "$LOCAL"; fi)" "$REMOTE"'
+git config --global mergetool.diffmerge.trustExitCode true
